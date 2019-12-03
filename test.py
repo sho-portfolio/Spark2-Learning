f362f669e86f34fcbf2883581ad054f1020c552a
sho-portfolio <40234177+sho-portfolio@users.noreply.github.com>
2019-12-03 12:47:22 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,16 +4,7 @@ from pyspark.sql import functions
 
 
 if __name__ == "__main__":
-    # Create a SparkSession (the config bit is only for Windows!)
-    spark = SparkSession.builder.appName("PopularMovies").getOrCreate()
-
-    # Get the raw data
-    lines = spark.sparkContext.textFile("hdfs:///user/maria_dev/ml-100k/u.data")
-    # Convert it to a RDD of Row objects with (movieID, rating)
-    movies = lines.map(parseInput)
-    # Convert that to a DataFrame
-    movieDataset = spark.createDataFrame(movies)
-
+   
     #sc.textFile("ml-100k/u.data") \
     #    .map(lambda line: line.split(",")) \
     #    .filter(lambda line: len(line)>1) \
