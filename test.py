0c2475cdccba11fde303592c9fd54858f2894b3f
sho-portfolio <40234177+sho-portfolio@users.noreply.github.com>
2019-12-03 12:46:31 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,9 +7,6 @@ if __name__ == "__main__":
     # Create a SparkSession (the config bit is only for Windows!)
     spark = SparkSession.builder.appName("PopularMovies").getOrCreate()
 
-    # Load up our movie ID -> name dictionary
-    movieNames = loadMovieNames()
-
     # Get the raw data
     lines = spark.sparkContext.textFile("hdfs:///user/maria_dev/ml-100k/u.data")
     # Convert it to a RDD of Row objects with (movieID, rating)
