425dc77d69e523b9ad5d9a63762819edab0bbf45
sho-portfolio <40234177+sho-portfolio@users.noreply.github.com>
2019-12-03 12:24:23 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,41 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.sql import functions
+
+def loadMovieNames():
+    movieNames = {}
+    with open("ml-100k/u.item") as f:
+        for line in f:
+            fields = line.split('|')
+            movieNames[int(fields[0])] = fields[1]
+    return movieNames
+
+def parseInput(line):
+    fields = line.split()
+    return Row(movieID = int(fields[1]), rating = float(fields[2]))
+
+if __name__ == "__main__":
+    # Create a SparkSession (the config bit is only for Windows!)
+    spark = SparkSession.builder.appName("PopularMovies").getOrCreate()
+
+    # Load up our movie ID -> name dictionary
+    movieNames = loadMovieNames()
+
+    # Get the raw data
+    lines = spark.sparkContext.textFile("hdfs:///user/maria_dev/ml-100k/u.data")
+    # Convert it to a RDD of Row objects with (movieID, rating)
+    movies = lines.map(parseInput)
+    # Convert that to a DataFrame
+    movieDataset = spark.createDataFrame(movies)
+
+
+
+    #df = spark.read.format("csv").option("header", "false").load("ml-100k/u.item")
+    #val df = spark.sql("SELECT * FROM csv.`hdfs:///csv/file/dir/file.csv`")
+
+    val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter", '|').load("ml-100k/u.item")
+
+
+
+    # Stop the session
+    spark.stop()
