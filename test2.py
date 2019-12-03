1770e94352820e8c64e87bfded09ea0a81ab1fe1
sho-portfolio <40234177+sho-portfolio@users.noreply.github.com>
2019-12-03 12:53:18 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,17 +1,11 @@
 from pyspark.sql import SparkSession
-from pyspark.sql import Row
-from pyspark.sql import functions
 
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
 
-if __name__ == "__main__":
-   
-    #sc.textFile("ml-100k/u.data") \
-    #    .map(lambda line: line.split(",")) \
-    #    .filter(lambda line: len(line)>1) \
-    #    .map(lambda line: (line[0],line[1])) \
-    #    .collect()    
+df = spark.read.format("csv").option("header", "false").load("ml-100k/u.data")
 
-    df = spark.read.format("csv").option("header", "false").load("ml-100k/u.data")
-
-    # Stop the session
-    spark.stop()
+df.show()
