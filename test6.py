13413777bb2227484048dfc3bdff297887dfefff
sho-portfolio <40234177+sho-portfolio@users.noreply.github.com>
2019-12-03 13:36:21 -0500

    Update test6.py

--- test6.py
+++ test6.py
@@ -20,7 +20,13 @@ df.createOrReplaceTempView("mydataset")
 sqlDF = spark.sql("SELECT * FROM mydataset")
 sqlDF.show()
 
+
+
+
+
 df2 = spark.read.format("csv").option("header", "false").option("delimiter", '|').load("ml-100k/u.item")
-
-
 df2.show()
+
+df2.createOrReplaceTempView("mydataset2")
+sqlDF2 = spark.sql("SELECT * FROM mydataset2")
+sqlDF2.show()
