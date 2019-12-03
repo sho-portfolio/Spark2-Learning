125f3b9d7d94372cee12345bd65f9457f4ca601f
sho-portfolio <40234177+sho-portfolio@users.noreply.github.com>
2019-12-03 13:58:59 -0500

    Update and rename test12.py to test13.py

--- test13.py
+++ test13.py
@@ -33,9 +33,9 @@ sqlDF.show()
 
 
 
-df2 = spark.read.format("csv").option("header", "false").option("delimiter", '|').load("ml-100k/u.item")
-df2.show()
+#df2 = spark.read.format("csv").option("header", "false").option("delimiter", '|').load("ml-100k/u.item")
+#df2.show()
 
-df2.createOrReplaceTempView("mydataset2")
-sqlDF2 = spark.sql("SELECT mydataset2._c0 FROM mydataset2")
-sqlDF2.show()
+#df2.createOrReplaceTempView("mydataset2")
+#sqlDF2 = spark.sql("SELECT mydataset2._c0 FROM mydataset2")
+#sqlDF2.show()
