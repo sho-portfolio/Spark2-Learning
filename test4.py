494f9cda46195ae742408ac40952d2c7ed2aeec2
sho-portfolio <40234177+sho-portfolio@users.noreply.github.com>
2019-12-03 12:57:36 -0500

    Create test4.py

---
+++ test4.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+df = spark.read.format("csv").option("header", "false").load("ml-100k/u.data")
+
+df.show()
+
+df.printSchema()
+
+
+df.createOrReplaceTempView("mydataset")
+sqlDF = spark.sql("SELECT * FROM mydataset")
+sqlDF.show()
